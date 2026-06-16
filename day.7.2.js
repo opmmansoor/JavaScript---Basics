d1c75518569e6ae8731228a16eb9dc0be98b5055
Muhammed Mansoor <mansoor@email.com>
2026-06-16 17:24:53 +0530

    Storage

---
+++ day.7.2.js
@@ -0,0 +1,11 @@
+//Local Storage
+//Local Storage is used to store data in the browser permanently.
+
+localStorage.setItem("name", "Samuel");
+console.log(localStorage.getItem("name"));
+
+
+
+//Session Storage
+sessionStorage.setItem("name", "Jabir");
+console.log(sessionStorage.getItem("name"));
