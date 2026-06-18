50b96704dbf1e998b0618729486e44273e4adf89
opmmansoor <opmmansoor@gmail.com>
2026-06-18 11:59:50 +0530

    STRING METHODs update

--- day.2.1.js
+++ day.2.1.js
@@ -1,19 +1,22 @@
 //STRING METHODs ( “A string is a sequence of characters used to store text in JavaScript.”)
-
+// buit-in functions; used to perform operation on STRING
 
 // 1.SPLIT [convert to Array]
-
 let items = "apple,orange,mango"
 // console.log(items);
 console.log(items.split(","));
 
 
-//2.slice() [extracts part]
+
+//2.slice() [extracts part] (start,end)
+// [start-->>>   , <<<--end ]
 let text = "happy day"
 console.log(text.slice(1,-5));
 
 
-//3. substring() [also extracts part of a string]
+
+//3. substring() [also extracts part of a string] (start,end)
+//[start,end-->>> ]
 let texts = "Java Script";
 console.log(texts.substring(0,5));
 
@@ -51,7 +54,8 @@ let call = "Mansoor";
 console.log(call.charAt(3));
 
 
-//9. replace()  [text replace]
+
+//9. replace()  [old teext replace to new text]
 let master = "how are you";
 console.log(master.replace("how are you","fine"));
 
@@ -65,5 +69,11 @@ console.log(topic.trim());
 
 //11. padStart() [ adding extra characters in beginning String]
 // string.padStart(totalLength, value)
+//a.
 let num = "5";
 console.log(num.padStart(3,"1"));
+
+
+//b.
+let count = "NE";
+console.log(count.padStart(3,"O"));
